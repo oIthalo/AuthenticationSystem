288e70a9f1dcab8d5061565f4af1fa7ec3654600
oIthalo <ithalobarreto333@gmail.com>
2024-10-06 06:23:42 -0300

    User authentication models

---
+++ AuthenticationSystem/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationSystem.Models;
+
+public class Role
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }    
+    public string Description { get; set; }
+    public List<User> Users { get; set; }
+}
