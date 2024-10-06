744078fdc43c1d0402bbd9549952613f24783df1
oIthalo <ithalobarreto333@gmail.com>
2024-10-06 07:21:58 -0300

    JWT configured

--- AuthenticationSystem/Program.cs
+++ AuthenticationSystem/Program.cs
@@ -1,5 +1,8 @@
 using AuthenticationSystem.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,26 @@ builder.Services.AddDbContext<AppDbContext>(opts => opts
     .UseMySql(connectionStr, ServerVersion
     .AutoDetect(connectionStr)));
 
+var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+var key = Encoding.ASCII.GetBytes(secretKey!);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = true,
+    };
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -18,6 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 app.Run();
---
+++ AuthenticationSystem/Services/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using AuthenticationSystem.Data.DataRequests;
+using AuthenticationSystem.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+namespace AuthenticationSystem.Services;
+
+public static class TokenService
+{
+    public static string GenerateToken(User model)
+    {
+        var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+        var key = Encoding.ASCII.GetBytes(secretKey!);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = GenerateClaims(model),
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddHours(2),
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public static ClaimsIdentity GenerateClaims(User model)
+    {
+        var ci = new ClaimsIdentity();
+
+        ci.AddClaim(new Claim(ClaimTypes.Name, model.Email));
+        ci.AddClaim(new Claim(ClaimTypes.Role, model.Role.ToString()!));
+
+        return ci;
+    }
+}
